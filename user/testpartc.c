a90197431d774cf6d24286b13acfef6a4b31f70a
grodriguez0629 <114261139+grodriguez0629@users.noreply.github.com>
2023-10-01 21:21:27 -0400

    Update testpartc.c

--- user/testpartc.c
+++ user/testpartc.c
@@ -4,6 +4,6 @@
 // running system call PartC()
  int main(void)
 {
-   printf(1, "There are %d system calls.\n", thirdpart());
+   printf(1, "There are %d system calls.\n", thirdpart()); //reports the number of SUCCESSFUL system calls
    exit();
 }
