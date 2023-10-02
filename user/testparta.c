86a73ffcd19d8a1b42320d48c0ff2e757b7da6f3
grodriguez0629 <114261139+grodriguez0629@users.noreply.github.com>
2023-10-01 21:18:27 -0400

    Update testparta.c

--- user/testparta.c
+++ user/testparta.c
@@ -4,11 +4,11 @@
 int
 main(void)
 {
-  printf(1, "Initial count: %d\n", firstpart());
+  printf(1, "Initial count: %d\n", firstpart()); //reports how many times getpid has been called (should be zero)
+  getpid(); //calls getpid
+  printf(1, "After 1 getpid(): %d\n", firstpart()); //reports updated count (should be one)
+  getpid(); 
   getpid();
-  printf(1, "After 1 getpid(): %d\n", firstpart());
-  getpid();
-  getpid();
-  printf(1, "After 3 getpid(): %d\n", firstpart());
-  exit();
+  printf(1, "After 3 getpid(): %d\n", firstpart()); //reports updated count (should be three now)
+  exit(); //ends the program
 }
